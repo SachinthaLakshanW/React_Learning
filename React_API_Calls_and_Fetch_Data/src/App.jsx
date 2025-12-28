5763fc3124d363de493422e0cdb494cfca7529dd
SachinthaLakshanW <sachinthalakshan333@gmail.com>
2025-12-28 13:12:48 +1300

    ExcuseAPI

--- React_API_Calls_and_Fetch_Data/src/App.jsx
+++ React_API_Calls_and_Fetch_Data/src/App.jsx
@@ -1,24 +1,28 @@
 import Axios from "axios";
 import "./App.css";
-import { useEffect, useState } from "react";
+import { useState } from "react";
 
 function App() {
-  const [catFact, setCatFact] = useState("");
+  const [generateExcuse, setGenerateExcuse] = useState("");
 
-  const fetchCatFact = () => {
-    Axios.get("https://catfact.ninja/fact").then((res) => {
-      setCatFact(res.data.fact);
-    });
+  const fetchExcuse = (excuse) => {
+    Axios.get(`https://excuser-three.vercel.app/v1/excuse/${excuse}/`).then(
+      (response) => {
+        setGenerateExcuse(response.data[0].excuse);
+      }
+    );
   };
-
-  useEffect(() => {
-    fetchCatFact();
-  }, []);
 
   return (
     <div className="App">
-      <button onClick={fetchCatFact}>Generate Cat Fact</button>
-      <p> {catFact}</p>
+      <h1>Generate an Excuse</h1>
+
+      <button onClick={() => fetchExcuse("party")}>Party</button>
+      <button onClick={() => fetchExcuse("family")}>Family</button>
+      <button onClick={() => fetchExcuse("office")}>Office</button>
+      <button onClick={() => fetchExcuse("children")}>Children</button>
+
+      <h2>{generateExcuse}</h2>
     </div>
   );
 }
